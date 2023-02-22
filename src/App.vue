5f47f6d72b6d2cd435c5bb42ac8270c502b8f22f
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-02-22 09:52:48 -0300

    navegacao

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <router-view />
+    <div class="container grid-lg">
+      <header class="navbar pt-2">
+        <section class="navbar-section">
+          <router-link to="/" class="btn btn-link">Todo</router-link>
+          <router-link to="/kanban" class="btn btn-link">Kanban</router-link>
+        </section>
+      </header>
+      <router-view />
+    </div>
   </div>
 </template>
 
